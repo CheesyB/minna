81e765cf855b2733ba7e053523d019302e0032c1
Tim Breu <tim.breu@mailbox.org>
2019-12-27 13:14:35 +0100

    moresql

--- minna/sqlite_adapter.py
+++ minna/sqlite_adapter.py
@@ -18,12 +18,26 @@ class SqlAdapter():
 
     @property
     def latest_list(self):
-        latest_id = self.c.execute(''' 
+        latest_id = self.c.execute('''
             SELECT id FROM lists
             ORDER BY id DESC
             LIMIT 1 ''').fetchone()[0]
         self.logger.info('current list id is {}'.format(latest_id))
         return latest_id
+
+    def getListPk(self, tag=None):
+        if tag:
+            list_pk = self.c.execute(' SELECT id FROM lists WHERE tag = ?',
+                    (tag,)).fetchone()
+            if not list_pk:
+                self.logger.error("tag {} does not exist".format(tag))
+                raise Exception("tag {} does not exist".format(tag))
+
+            self.logger.info("list_pk is {}".format(list_pk))
+            return list_pk
+        else:
+            return self.latest_list
+
 
     def initDB(self):
         self.c.execute('''
@@ -38,7 +52,8 @@ class SqlAdapter():
         self.c.execute('''
             CREATE TABLE IF NOT EXISTS items (
             id INTEGER NOT NULL PRIMARY KEY,
-            item VARCHAR(30) NOT NULL UNIQUE
+            item VARCHAR(30) NOT NULL UNIQUE,
+            times_used INT NOT NULL
             ); '''
                        )
         self.logger.info("created table items")
@@ -55,60 +70,54 @@ class SqlAdapter():
 
     def newList(self, tag=None):
         self.c.execute(''' INSERT INTO lists (timestamp,tag) values(?,?)''',
-                (time.time(), tag))
-        self.logger.info('new list added')
+                       (time.time(), tag))
+        self.logger.info('new list added {}'.format(tag))
 
-    def addItems(self, items, tag=None):
+    def addItems(self, items, list_pk):
+        if( list_pk > self.latest_list or list_pk < 1 ):
+            raise Exception("list_pk {} out of range".format(list_pk))
+        
         for item in items:
             try:
-                self.c.execute('INSERT INTO items(item) values(?)', (item,))
-                self.logger.info('added {} to items'.format(item))
+                self.c.execute(
+                    ''' INSERT INTO items (item, times_used)
+                        VALUES(?,1)
+                        ON CONFLICT(item)
+                        DO UPDATE SET times_used = times_used + 1''', (item,))
+                self._mapItemToList(item, list_pk)
             except Exception as e:
-                self.logger.info(
-                    '{} \nitem: {} already exists'.format(e, item))
-        
-        try:
-            item_keys = self.c.execute(
-                '''SELECT id FROM items WHERE item IN ({})'''.format(', '.join('?' * len(items))), items).fetchall()
-            self.logger.info(
-                'primary keys {}'.format(item_keys))
-        except Exception as e:
-            self.logger.info(e)
-        
-        try:
-            self.c.executemany('''INSERT INTO items_lists (item, list )
-                    values(?,?)''',
-                    [(ik[0], self.latest_list) for ik in item_keys]
-            )
-        except Exception as e:
-            self.logger.info(e)
-        
+                self.logger.info(e)
+                raise e
 
+    def _mapItemToList(self, item, list_pk):
+        item_pk = self.c.execute(
+            '''SELECT id FROM items WHERE item = ? ''', (item,)).fetchone()[0]
+        self.c.execute('''INSERT INTO items_lists (item, list )
+                values(?,?)''', (item_pk, list_pk))
+        self.logger.info('added {} to {}'.format(
+            item, list_pk))
 
 
     def insertStuff(self):
-        self.c.execute(
-            'INSERT INTO lists(tag, timestamp) values (?, ?)', [
-                "test", int(time.time())]
-        )
-        self.c.execute(
-            'INSERT INTO lists(tag, timestamp) values (?, ?)',
-            ["tim", int(time.time())]
-        )
-        self.c.execute('INSERT INTO lists(tag, timestamp) values (?, ?)', [
-                       "anna", time.time()])
 
-        mylist=[("Tomate",), ("Banane",), ("Kaffee",),
-                  ("Grünkohl",), ("Bier",)]
 
-        self.c.executemany('INSERT INTO items(item) values (?)', mylist)
-        self.c.executemany('INSERT INTO items_lists(item, list) values (?, ?)', [
-            (1, 1), (2, 1), (3, 1), (5, 1), (1, 2), (3, 2), (4, 2)])
 
         self.newList("tutiuti")
+        self.newList("TimTest")
+        self.newList("Paris")
+        self.newList("Einkauf")
+        
+        self.addItems(["Nudeln", "Bohnen", "Gurken", "Tomate"], 2)
+        self.addItems(["Nudeln", "Banane", "Gurken", "Tomate"], 1)
+        self.addItems(["Nudeln", "Apfel", "Gurken", "Tomate"], 4)
+        try:
+            self.addItems(["Nudeln", "Apfel", "Gurken", "Tomate"], 0)
+        except Exception as e:
+            print(e)
+        
 
     def getStuff(self):
-        print(self.c.execute('SELECT * FROM items').fetchall())
+        print("Items: {}".format(self.c.execute('SELECT * FROM items').fetchall()))
         print("Lists: {}".format(self.c.execute('SELECT * FROM lists').fetchall()))
         print(self.c.execute('''
             SELECT i.item FROM items as i
@@ -121,11 +130,8 @@ if __name__ == "__main__":
 
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                         level=logging.DEBUG)
-    connection = sqlite3.connect(':memory:')
-    sqlAdaper = SqlAdapter(connection)
+    connection=sqlite3.connect(':memory:')
+    sqlAdaper=SqlAdapter(connection)
     sqlAdaper.insertStuff()
-    sqlAdaper.addItems(["Tomate", "Makrelen", "Tim", "Tim_Tom"])
+    sqlAdaper.addItems(["Tomate", "Makrelen", "Tim", "Tim_Tom"],3)
     sqlAdaper.getStuff()
-
-
-
